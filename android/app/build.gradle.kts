875ce05b4caa869469ff517bcd937f7aeb72b186
Cat <git@chen.rs>
2026-09-08 10:03:56 +0800

    分享不进最近任务，从历史点回来不再转

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "onl.nl0.chengshu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
     signingConfigs {
         create("stable") {
